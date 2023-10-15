7e9079628f78858f0eaf02b238c56047988a42cb
Elena Spirina <espirina@list.ru>
2023-10-15 10:59:00 +0400

    initial commit

---
+++ src/components/ListViewFunc.js
@@ -0,0 +1,13 @@
+import ShopItem from "./ShopItemFunc";
+
+export default function ListView(props) {
+  const { items } = props;
+
+  return (
+    <div className="item-list">
+      {items.map((item, index) => (
+        <ShopItem item={item} key={index} />
+      ))}
+    </div>
+  );
+}
